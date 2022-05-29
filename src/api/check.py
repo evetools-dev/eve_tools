5e9ae0619d66bec657c7b4e0b8acc138e1ddf925
Hanbo <hbopublic@163.com>
2022-05-29 02:33:23 -0500

    Add check_type_id function to api.
    Checks if a type_id is valid. Invalid type_ids will cause 404 error on
    ESI endpoints that has type_id field. Consecutive 404 errors will cause
    502 errors from ESI because of reaching error_limit headers.
    
    If applied, this code will not affect other code.

---
+++ src/api/check.py
@@ -0,0 +1,29 @@
+from src.ESI import ESIClient
+
+
+def check_type_id(type_id: int) -> bool:
+    """Checks if a type_id is valid.
+
+    A type_id is "valid" if other apis requring type_id field do not give 404 errors.
+
+    Checks using /universe/types/{type_id}/ endpoint, which returns information of type_id.
+    There are lots of fields in response, and only uses "published" field as valid flag.
+    "published" field is present in all responses from this endpoint.
+
+    Args:
+        type_id: A int that specifies the type_id to check.
+
+    Returns:
+        A bool that shows if type_id is valid.
+    """
+    resp = ESIClient.get("/universe/types/{type_id}/", type_id=type_id)
+    valid = resp.get("published")
+    return valid
+
+
+async def _check_type_id_async(type_id: int) -> bool:
+    """Coroutine version of check_type_id(). See check_type_id().
+    """
+    resp = await ESIClient.request("get", "/universe/types/{type_id}/", type_id=type_id)
+    valid = resp.get("published")
+    return valid
